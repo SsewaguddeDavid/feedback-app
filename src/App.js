f678efd546a12af5f8b46b958bce5c2d14887dba
Ssewagudde David <dsewagude10@gmail.com>
2022-10-31 23:02:43 +0300

    modified form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { v4 as uuidv4 } from 'uuid'
+
 import { Header } from './components/Header';
 import {FeedbackList} from './components/FeedbackList';
 import { FeedbackStats } from './components/FeedbackStats';
+import { FeedbackForm } from './components/FeedbackForm';
 import FeedbackData from './data/FeedbackData'
 import {useState} from 'react'
 import './App.css';
@@ -13,14 +16,20 @@ function App() {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
   return (
     <>
     <Header />
     <div className="container">
+      <FeedbackForm handleAdd={addFeedback}/>
       <FeedbackStats feedback = {feedback}/>
       <FeedbackList 
       feedback = {feedback}
-      handleDelete = {deleteFeedback}
+      handleDelete = { deleteFeedback }
       />
     </div>
     </>
