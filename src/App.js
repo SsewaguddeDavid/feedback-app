2a1674ae9a17edd14fee2c2cd96414dc19e5b4b5
Ssewagudde David <dsewagude10@gmail.com>
2022-11-03 22:29:52 +0300

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { v4 as uuidv4 } from 'uuid'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 import { Header } from './components/Header';
 import {FeedbackList} from './components/FeedbackList';
 import { FeedbackStats } from './components/FeedbackStats';
 import { FeedbackForm } from './components/FeedbackForm';
 import FeedbackData from './data/FeedbackData'
+import { AboutIconLink } from './components/AboutIconLink';
+import { AboutPage } from './pages/AboutPage';
 import {useState} from 'react'
 import './App.css';
 
@@ -22,17 +25,27 @@ function App() {
     setFeedback([newFeedback, ...feedback])
   }
   return (
-    <>
-    <Header />
-    <div className="container">
-      <FeedbackForm handleAdd={addFeedback}/>
-      <FeedbackStats feedback = {feedback}/>
-      <FeedbackList 
-      feedback = {feedback}
-      handleDelete = { deleteFeedback }
-      />
-    </div>
-    </>
+    <Router>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route exact path='/' element = {
+            <>
+              <FeedbackForm handleAdd={addFeedback}/>
+              <FeedbackStats feedback = {feedback}/>
+              <FeedbackList 
+              feedback = {feedback}
+              handleDelete = { deleteFeedback }
+            />
+            </>
+          }>
+            
+          </Route>
+          <Route path='/about' element={<AboutPage/>}/>
+        </Routes>
+      </div>
+      <AboutIconLink />
+    </Router>
     
   );
 }
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card } from '../components/shared/Card'
+import { Link } from "react-router-dom"
+
+export const AboutPage = () => {
+  return (
+    <Card>
+        <h1>About this project</h1>
+        <p>This is a react app to leave feedback for a product or a service</p>
+        <p>Version: 1.0.0</p>
+        <p><Link to="/">Back to home</Link></p>
+    </Card>
+  )
+}
